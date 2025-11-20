0e69a2e6fbd3416e2f7b69bbd0408e1fab3bd64f
“Zeeshan05171” <zeeshanhyder012@gmail.com>
2025-11-21 01:43:37 +0500

    Nested For Loop

---
+++ practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    for (int a=1;a<10;a++){
+        cout<<"\n";
+        for(int b=1;b<a;b++){
+            cout<<"*";
+        }
+    }
+    return 0;
+}
